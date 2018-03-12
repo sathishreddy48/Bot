6eeee3a87cbaa9fc10d777ca81ecee96b178aa19
Sathish Reddy <sathish.reddy@procode.in>
2018-03-12 10:03:54 +0530

    updating project files

---
+++ TravelReq-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=travelreq-Web-Deploy.pubxml -p:Password=eskMBM4mMz1Fj57EZNSqtKtwj2gcmgl8w61k3aZZgQH3EZhrqS1kQAtHJ0ko
+
